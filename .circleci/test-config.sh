7e49660da4dfbb0479fd1f2152ad52b71d5ad9c8
Greg Reinbach <greg@reinbach.com>
2018-05-18 13:48:11 -0400

    Update circleci test config base revision

--- .circleci/test-config.sh
+++ .circleci/test-config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 curl --user ${CIRCLE_TOKEN}: \
     --request POST \
-    --form revision=ac93c0a3147145eed8db0614bad4f00ba7c2ba0e \
+    --form revision=b6989c3410ee204551fa84a0ef34af630f7bf91e \
     --form config=@config.yml \
     --form notify=false \
         https://circleci.com/api/v1.1/project/github/unicef/etools-validator/tree/master
