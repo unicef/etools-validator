7481b929c59ceeb4a546c25a755ae1bf739b8c78
Greg Reinbach <greg@reinbach.com>
2019-09-26 08:11:14 -0400

    Standardize setup for libraries

--- .circleci/test-config.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-curl --user ${CIRCLE_TOKEN}: \
-    --request POST \
-    --form revision=b6989c3410ee204551fa84a0ef34af630f7bf91e \
-    --form config=@config.yml \
-    --form notify=false \
-        https://circleci.com/api/v1.1/project/github/unicef/etools-validator/tree/master
