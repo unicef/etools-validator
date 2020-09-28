9b7ae96e3fcafb34ad2f9beba2742854794bd57a
Greg Reinbach <greg@reinbach.com>
2020-09-28 15:37:16 -0400

    Update isort config, based on new version

--- src/etools_validator/validation.py
+++ src/etools_validator/validation.py
@@ -3,6 +3,7 @@ import logging
 
 from django.apps import apps
 from django.utils.functional import cached_property
+
 from django_fsm import can_proceed, get_all_FIELD_transitions, has_transition_perm
 
 from .decorators import error_string, state_error_string, transition_error_string
